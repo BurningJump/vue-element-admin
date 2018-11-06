dac7f2cb383da5fb2b94936b7fd979905267d1f7
BurningJump <935646729@qq.com>
2018-11-06 23:34:56 +0800

    tree节点获取

--- src/views/permission/permissionList.vue
+++ src/views/permission/permissionList.vue
@@ -3,13 +3,15 @@
     <el-container>
       <el-header height="auto">查询条件</el-header>
       <el-main>
-        <el-form v-for="condition in qCondition" :key="condition.label" :style="{width: condition.width*100 + '%'}" class="demo-ruleForm" label-width="100px" size="mini">
-          <el-form-item :label="condition.label">
-            <el-input/>
+        <el-form :model="conditionForm" ref="conditionForm" class="demo-ruleForm" label-width="100px" size="mini">
+          <el-form-item v-for="condition in qCondition" :key="condition.label" :style="{width: condition.width*100 + '%'}" :label="condition.label" :prop="conditionForm[condition.findField]">
+            <el-input v-model="conditionForm[condition.findField]"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" icon="el-icon-search">查询</el-button>
+            <el-button type="primary" @click="resetForm()">重置</el-button>
           </el-form-item>
         </el-form>
-        <el-button type="primary" icon="el-icon-search">查询</el-button>
-        <el-button type="primary">重置</el-button>
       </el-main>
     </el-container>
     <div class="toolbar">
@@ -23,10 +25,10 @@
     </div>
     <el-container>
       <el-aside width="200px">
-        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <el-tree :data="tree" :props="defaultProps" @node-expand="handleNodeExpand" @node-click="handleNodeClick"></el-tree>
       </el-aside>
       <el-main>
-        <el-table ref="multipleTable" :data="list" element-loading-text="拼命加载中" border fit highlight-current-row>
+        <el-table ref="multipleTable" expand-on-click-node :data="list" element-loading-text="拼命加载中" border fit highlight-current-row>
           <el-table-column type="selection" align="center"/>
           <el-table-column label="行号" align="center">
             <template slot-scope="scope">
@@ -52,6 +54,9 @@ export default{
     return {
       listUI: require('./list-ui.json'),
       listData: require('./list-data.json'),
+      treeRoot: require('./root.json'),
+      treeChild: require('./child.json'),
+      treeGrandchild: require('./grandchild.json'),
       qCondition: [],
       buttons: [],
       tree: [],
@@ -62,15 +67,17 @@ export default{
         limit: 20
       },
       multipleSelection: [],
-      data: [{
-        label: '一级 1',
-        children: [{
-          label: '二级 1-1',
+      data: [
+        {
+          label: '一级 1',
           children: [{
-            label: '三级 1-1-1'
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
           }]
-        }]
-      }, {
+        },
+        {
         label: '一级 2',
         children: [{
           label: '二级 2-1',
@@ -83,7 +90,8 @@ export default{
             label: '三级 2-2-1'
           }]
         }]
-      }, {
+      },
+      {
         label: '一级 3',
         children: [{
           label: '二级 3-1',
@@ -100,7 +108,8 @@ export default{
       defaultProps: {
         children: 'children',
         label: 'label'
-      }
+      },
+      conditionForm: {}
     }
   },
   mounted() {
@@ -110,13 +119,19 @@ export default{
   methods: {
     getUIdata() {
       this.qCondition = [].concat(this.listUI.listViewModel.qCondition.components)
+      this.qCondition.forEach((condition) => {
+        this.conditionForm[condition.findField] = ''
+      })
       this.buttons = [].concat(this.listUI.listViewModel.toolbar.buttons)
-      this.tree = [].concat(this.listUI.listViewModel.tree)
       this.listUI.listViewModel.grid.components.forEach((item) => {
         this.grid.push({
           prop: item.name,
           label: item.label
         })
+      })
+      this.tree.push({
+        label: this.treeRoot.treeRoot.text,
+        children: []
       })
     },
     getListData() {
@@ -137,8 +152,53 @@ export default{
     handleSelectionChange(val) {
       this.multipleSelection = val
     },
-    handleNodeClick(data) {
-      console.log(data)
+    handleNodeExpand(data, node, ref) {
+      // 展开节点
+      // console.log(data,node,ref)
+    },
+    handleNodeClick(data, node, ref) {
+      // 点击节点时获取子节点及表数据
+      console.log(data, node, ref)
+      if (data.children && data.children.length > 0) {
+        // 已加载过节点
+        return
+      }
+      if (data.children && data.children.length === 0) {
+        // 获取子节点
+        if (node.level === 1) {
+          this.treeChild.forEach((child) => {
+            if (!child.leaf) {
+              this.tree[0].children.push({
+                label: child.text,
+                children: []
+              })
+            } else {
+              this.tree.children.push({
+                label: child.text
+              })
+            }
+          })
+        } else if (node.level === 2) {
+          this.treeGrandchild.forEach((grandchild) => {
+            if (!grandchild.leaf) {
+              this.tree[0].children[0].children.push({
+                label: grandchild.text,
+                children: []
+              })
+            } else {
+              this.tree[0].children[0].children.push({
+                label: grandchild.text
+              })
+            }
+          })
+        }
+      }
+      if (node.isLeaf) {
+        // 叶节点，加载表数据
+      }
+    },
+    resetForm() {
+      Object.keys(this.conditionForm).forEach(key => obj[key] = '');
     }
   }
 }
