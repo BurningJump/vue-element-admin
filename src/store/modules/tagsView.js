e6d15fc6400317cff107d687e14173feb53b82da
burningjump <935646729@qq.com>
2019-02-22 16:54:47 +0800

    tag可以显示多个相同页面

--- src/store/modules/tagsView.js
+++ src/store/modules/tagsView.js
@@ -5,7 +5,8 @@ const tagsView = {
   },
   mutations: {
     ADD_VISITED_VIEW: (state, view) => {
-      if (state.visitedViews.some(v => v.path === view.path)) return
+      // 防止重复
+      // if (state.visitedViews.some(v => v.path === view.path)) return
       state.visitedViews.push(
         Object.assign({}, view, {
           title: view.meta.title || 'no-name'
